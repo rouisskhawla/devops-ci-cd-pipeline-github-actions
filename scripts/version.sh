4ebcd55438c46d613695dbfe71b96ced4d8ffde3
Khawla Rouis <38538299+rouisskhawla@users.noreply.github.com>
2025-11-23 16:20:48 +0000

    snapshot counter

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,7 +3,6 @@ set -e
 
 BUMP=${1:-patch}
 RELEASE_TYPE=${2:-snapshot}
-GITHUB_RUN_NUMBER=${GITHUB_RUN_NUMBER:-0}
 
 git fetch --tags
 
@@ -13,38 +12,47 @@ if [[ -z "$LAST_RELEASE_TAG" ]]; then
 fi
 BASE_VERSION=${LAST_RELEASE_TAG#v}
 
-MAJOR=$(echo $BASE_VERSION | cut -d. -f1)
-MINOR=$(echo $BASE_VERSION | cut -d. -f2)
-PATCH=$(echo $BASE_VERSION | cut -d. -f3)
+LAST_SNAPSHOT_FILE="snapshot-version.txt"
 
 if [[ "$RELEASE_TYPE" == "release" ]]; then
+  MAJOR=$(echo $BASE_VERSION | cut -d. -f1)
+  MINOR=$(echo $BASE_VERSION | cut -d. -f2)
+  PATCH=$(echo $BASE_VERSION | cut -d. -f3)
+  
   case $BUMP in
     major) MAJOR=$((MAJOR+1)); MINOR=0; PATCH=0 ;;
     minor) MINOR=$((MINOR+1)); PATCH=0 ;;
     patch) PATCH=$((PATCH+1)) ;;
     *) echo "Invalid bump type: $BUMP"; exit 1 ;;
   esac
-  NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
-else
-  LAST_SNAPSHOT_TAG=$(git tag --sort=-creatordate | grep "v$BASE_VERSION-SNAPSHOT" | head -n1)
 
-  if [[ -z "$LAST_SNAPSHOT_TAG" ]]; then
-    SNAPSHOT_COUNTER=1
+  NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+  TAG_NAME="v$NEW_VERSION"
+
+  git config user.name "github-actions"
+  git config user.email "actions@github.com"
+  git tag -a "$TAG_NAME" -m "Release $TAG_NAME"
+  git push origin "$TAG_NAME"
+
+  echo "BASE_VERSION=$NEW_VERSION" > $LAST_SNAPSHOT_FILE
+  echo "COUNTER=0" >> $LAST_SNAPSHOT_FILE
+
+else
+  if [[ -f "$LAST_SNAPSHOT_FILE" ]]; then
+    source $LAST_SNAPSHOT_FILE
+    if [[ "$BASE_VERSION" != "$BASE_VERSION" ]]; then
+      COUNTER=1
+    else
+      COUNTER=$((COUNTER+1))
+    fi
   else
-    SNAPSHOT_COUNTER=$(echo $LAST_SNAPSHOT_TAG | sed -E 's/^v[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT\.([0-9]+)$/\1/')
-    SNAPSHOT_COUNTER=$((SNAPSHOT_COUNTER+1))
+    COUNTER=1
   fi
 
-  NEW_VERSION="${BASE_VERSION}-SNAPSHOT.${SNAPSHOT_COUNTER}"
+  NEW_VERSION="${BASE_VERSION}-SNAPSHOT.${COUNTER}"
+  echo "BASE_VERSION=$BASE_VERSION" > $LAST_SNAPSHOT_FILE
+  echo "COUNTER=$COUNTER" >> $LAST_SNAPSHOT_FILE
 fi
 
-TAG_NAME="v$NEW_VERSION"
-
-git config user.name "github-actions"
-git config user.email "actions@github.com"
-
-git tag -a "$TAG_NAME" -m "${RELEASE_TYPE^} $TAG_NAME"
-git push origin "$TAG_NAME"
-
 echo "version=$NEW_VERSION" >> $GITHUB_OUTPUT
-echo "Git tag created: $TAG_NAME"
+echo "Calculated version: $NEW_VERSION"
