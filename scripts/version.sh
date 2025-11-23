fd160c7014cd7f19d6f2813731e9d1aee1cd620a
Khawla Rouis <38538299+rouisskhawla@users.noreply.github.com>
2025-11-23 19:58:19 +0000

    fix err

--- scripts/version.sh
+++ scripts/version.sh
@@ -14,18 +14,49 @@ LAST_RELEASE_VERSION=${LAST_RELEASE_TAG#v}
 
 LAST_SNAPSHOT_FILE="snapshot-version.txt"
 
-if [[ -f "$LAST_SNAPSHOT_FILE" ]]; then
-  source $LAST_SNAPSHOT_FILE
-  if [[ "$BASE_VERSION" != "$BASE_VERSION" ]]; then
+if [[ ! -f "$LAST_SNAPSHOT_FILE" ]]; then
+  echo "BASE_VERSION=$LAST_RELEASE_VERSION" > "$LAST_SNAPSHOT_FILE"
+  echo "COUNTER=0" >> "$LAST_SNAPSHOT_FILE"
+fi
+
+if [[ "$RELEASE_TYPE" == "release" ]]; then
+  MAJOR=$(echo $LAST_RELEASE_VERSION | cut -d. -f1)
+  MINOR=$(echo $LAST_RELEASE_VERSION | cut -d. -f2)
+  PATCH=$(echo $LAST_RELEASE_VERSION | cut -d. -f3)
+
+  case $BUMP in
+    major) MAJOR=$((MAJOR+1)); MINOR=0; PATCH=0 ;;
+    minor) MINOR=$((MINOR+1)); PATCH=0 ;;
+    patch) PATCH=$((PATCH+1)) ;;
+    *) echo "Invalid bump type: $BUMP"; exit 1 ;;
+  esac
+
+  NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+  TAG_NAME="v$NEW_VERSION"
+
+  git config user.name "github-actions"
+  git config user.email "actions@github.com"
+  git tag -a "$TAG_NAME" -m "Release $TAG_NAME"
+  git push origin "$TAG_NAME"
+
+  echo "BASE_VERSION=$NEW_VERSION" > "$LAST_SNAPSHOT_FILE"
+  echo "COUNTER=0" >> "$LAST_SNAPSHOT_FILE"
+
+else
+  source "$LAST_SNAPSHOT_FILE"
+
+  if [[ "$BASE_VERSION" != "$LAST_RELEASE_VERSION" ]]; then
+    BASE_VERSION=$LAST_RELEASE_VERSION
     COUNTER=1
   else
     COUNTER=$((COUNTER+1))
   fi
-else
-  COUNTER=1
+
+  NEW_VERSION="${BASE_VERSION}-SNAPSHOT.${COUNTER}"
+
+  echo "BASE_VERSION=$BASE_VERSION" > "$LAST_SNAPSHOT_FILE"
+  echo "COUNTER=$COUNTER" >> "$LAST_SNAPSHOT_FILE"
 fi
 
-
-if [[ "$RELEASE_TYPE" == "release" ]]; then
-  MAJOR=$(echo $LAST_RELEASE_VERSION | cut -d. -f1)
-  MINOR=$
+echo "version=$NEW_VERSION" >> $GITHUB_OUTPUT
+echo "Calculated version: $NEW_VERSION"
